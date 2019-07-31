9ae8246a11833ed12e5f2f4ce9f2d97d0558aada
dheeraj <djshres@gmail.com>
2019-07-31 16:20:55 +0545

    Initial

---
+++ DateConversion/NepDate.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DateConversion
+{
+    public class NepDate
+    {
+        public int Year { get; set; }
+        public int Month { get; set; }
+        public int Day { get; set; }
+
+        private string _weekDayName;
+        public string WeekDayName
+        {
+            get { return _weekDayName; }
+            set { _weekDayName = value; }
+        }
+
+        public string MonthName { get; set; }
+        public int WeekDay { get; set; }
+
+        public override string ToString()
+        {
+            return string.Format("{0}/{1}/{2}", Year, Month, Day);
+        }
+        public string ToLongDateString()
+        {
+            return string.Format("{0}, {1}, {2}, {3}", WeekDayName, MonthName, Day, Year);
+        }
+    }
+}
---
+++ Test/Program.cs
@@ -0,0 +1,18 @@
+﻿using DateConversion;
+using System;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine(Converter.EngToNep(DateTime.Now));
+            Console.WriteLine(Converter.NepToEng(new NepDate {
+                Year=2053,
+                Month=6,
+                Day=28
+            }));
+        }
+    }
+}
